13870fec8cd3b0ba7dff646237b3ba7d87ccde7c
DanyLass <danidias.schilling@gmail.com>
2021-09-14 19:57:50 -0300

    funcionalidade da tela registro

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,25 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form } from '@unform/web';
 
 import { Container, Content } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
 function Register() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    function handleName() {
+        console.log(name);
+        setName(name);
+    }
+
+    function handleEmail() {
+        setEmail(email);
+    }
+
+    function handlePassword() {
+        setPassword(password);
+    }
+    function handleConfirmPassword() {
+        setConfirmPassword(confirmPassword);
+    }
+
+    function handleOnClick() {}
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="FCamara" />
-                <form>
-                    <input placeholder="nome" />
+                <Form onSubmit={Register}>
+                    <input
+                        placeholder="Nome"
+                        onChange={handleName}
+                        type="nome"
+                    />
 
-                    <input placeholder="Email" />
+                    <input
+                        placeholder="Email"
+                        type="email"
+                        onChange={handleEmail}
+                    />
 
-                    <input type="password" placeholder="Senha" />
+                    <input
+                        placeholder="Senha"
+                        type="password"
+                        onChange={handlePassword}
+                    />
 
-                    <input type="password" placeholder="Confirmar Senha" />
+                    <input
+                        placeholder="Confirmar Senha"
+                        type="password"
+                        onChange={handleConfirmPassword}
+                    />
 
                     <button type="submit">Cadastrar</button>
-                </form>
+                </Form>
             </Content>
         </Container>
     );
